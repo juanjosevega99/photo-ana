99dc4cdff8822edce122b2ead67f741ebf7b7d4e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-18 16:11:53 -0500

    component timeline

---
+++ src/components/Card.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Comments from './Comments.svelte'
+</script>
+
+<div class="Card">
+  <div class="Card-container">
+    <div class="Card-header">
+      <div class="Card-user">
+        <img src="https://instagram.fnva1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/93571183_700157570811057_7056976415194873856_n.jpg?_nc_ht=instagram.fnva1-1.fna.fbcdn.net&_nc_ohc=_cTj6kBSRSEAX_mFVf2&oh=737d31baa078beb7c27db72698b2ba39&oe=5EC6C750" alt="">
+        <h2>
+          manuelaarias_98
+          <span>Huila, Colombia</span>
+        </h2>
+      </div>
+      <div class="Card-settings">
+        <i class="fas fa-ellipsis-h" />
+      </div>
+    </div>
+    <div class="Card-photo">
+      <figure>
+        <img src="https://instagram.fnva1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/93571183_700157570811057_7056976415194873856_n.jpg?_nc_ht=instagram.fnva1-1.fna.fbcdn.net&_nc_ohc=_cTj6kBSRSEAX_mFVf2&oh=737d31baa078beb7c27db72698b2ba39&oe=5EC6C750" alt="">
+      </figure>
+    </div>
+    <div class="Card-icons">
+      <div class="Card-icons-firsts">
+        <i class="fas fa-heart" />
+        <i class="fas fa-paper-plane" />
+      </div>
+      <div class="Card-icons-second">
+        <i class="fas fa-bookmark" />
+      </div>
+    </div>
+    <div class="Card-description">
+      <h3>manuelaarias_98</h3>
+      <span>Estudio, tomo café y leo; cazo atardeceres y tengo flores amarillas enterradas bajo la piel.</span>
+    </div>
+    <Comments />
+  </div>
+</div>
